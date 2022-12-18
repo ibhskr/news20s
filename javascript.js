cc1c3d519b7ddda36fbe8a311083e0ff85cf959a
Bhaskar Roy <112053263+ibhskr@users.noreply.github.com>
2022-12-19 00:21:12 +0530

    12-19-2022

---
+++ javascript.js
@@ -0,0 +1,69 @@
+
+var win;
+function news01(){
+  win = window.open("","_parent");
+}
+function news01(){
+  alert("Redirecting to anandabazar");
+  win = window.open("https://www.anandabazar.com","_parent");
+}
+function news02(){
+  alert("Redirecting to uttarbangasambad")
+  win = window.open("https://www.uttarbangasambad.com","_parent");
+}
+function news03(){
+  alert("Redirecting to aajkaal")
+  win = window.open("https://aajkaal.in","_parent");
+}
+
+function news04(){
+  alert("Redirecting to bartaman...")
+  win = window.open("https://bartamanpatrika.com/home","_parent");
+}
+function news05(){
+  alert("Redirecting to uttarersaradin...");
+  win = window.open("http://uttarersaradin.com/Default.aspx","_parent");
+}
+function news06(){
+  alert("Redirecting to Siliguritimes...");
+  win = window.open("https://be.siliguritimes.com","_parent");
+}
+function news07(){
+  alert("Redirecting to sangbadpratidin...")
+  win = window.open("https://www.sangbadpratidin.in","_parent");
+}
+function news08(){
+  alert("Redirecting to eisamay...")
+  win = window.open("https://eisamay.com","_parent");
+}
+function news09(){
+  alert("Redirecting to news18...")
+  win = window.open("https://bengali.news18.com","_parent");
+}
+function news10(){
+  alert("Redirecting to oneindia...")
+  win = window.open("https://bengali.oneindia.com","_parent");
+}
+function news11(){
+  alert("Redirecting to BBC Bengali...")
+  win = window.open("https://www.bbc.com/bengali","_parent");
+}
+function news12(){
+  alert("Redirecting to prothomalo...")
+  win = window.open("https://www.prothomalo.com","_parent");
+}
+
+function news13(){
+  alert("Redirecting to tv9bangla...")
+  win = window.open("https://tv9bangla.com/","_parent");
+}
+
+function news14(){
+  alert("Redirecting to hindustantimes...")
+  win = window.open("https://bangla.hindustantimes.com","_parent");
+}
+
+function news15(){
+  alert("Redirecting to ganashakti...")
+  win = window.open("https://bangla.ganashakti.co.in","_parent");
+}
